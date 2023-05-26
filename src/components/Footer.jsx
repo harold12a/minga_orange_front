b1e2439cb992f549e128a2f65a57807ddd0a2316
harold alzate <haroldalzate99@gmail.com>
2023-05-25 20:38:00 -0500

    agregando img al footer del mobiel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,35 @@ import {
   RiYoutubeFill,
   RiFacebookBoxFill,
   RiHeartLine,
-  RiVimeoFill
+  RiVimeoFill,
 } from "react-icons/ri";
 
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className="  fixed bottom-0 inset-x-0 flex  items-center gap-x-4 mb-4 justify-center  xl:hidden   ">
-          <button className=" text-4xl  text-white">
+        <div className="bg-white mt-[80%]   xl:hidden  md:flex md:mr-[50%] "> 
+          <img
+            src="../../src/assets/images/Frame 34648.png"
+            alt=""
+            className=" mx-auto pb-[50px]     "
+          />
+         <div className=" absolute pb-[7px]   inset-x-0 flex  items-center gap-x-4 md:flex md:ml-[50%]  justify-center  xl:hidden   ">
+          <button className=" text-4xl  text-black">
             <RiInstagramLine />
           </button>
-          <button className=" text-4xl  text-white">
+          <button className=" text-4xl  text-black">
             <RiFacebookCircleLine />
           </button>
-          <button className=" text-4xl  text-white">
+          <button className=" text-4xl  text-black">
             <RiTwitterFill />
           </button>
-          <button className=" text-4xl  text-white">
+          <button className=" text-4xl  text-black">
             <RiYoutubeFill />
           </button>
+          </div>
         </div>
+
         <div className="bottom-0   hidden xl:block overflow-x-hidden    ">
           <img
             src="../../src/assets/images/Rectangle14.png"
@@ -35,8 +43,12 @@ const Footer = () => {
             style={{ borderRadius: "0 0 140% 140%" }}
           />
 
-          {/* <div className=" h-[200px] mx-auto w-full  bg-[#4338CA] mb-[30px] rounded-[5px]  "> */}
           <div className=" h-[200px] mx-auto w-full  mb-[100px]    overflow-hidden ">
+            <img
+              src="../../src/assets/images/wave_white (1).svg"
+              alt=""
+              className="absolute mt-[-240px] "
+            />
             <div className="flex  ">
               <a href="" className="mr-[1%] ml-[15%] mt-[8%] absolute">
                 home
@@ -58,7 +70,7 @@ const Footer = () => {
               </div>
               <button className=" flex text-white items-center ml-[73%] mt-[9%]   w-[15%] h-10 px-[80px] bg-[#5E52F3] rounded-[50px] absolute ">
                 Donate <RiHeartLine />
-              </button> 
+              </button>
             </div>
             <hr className="border-b border-gray-400 my-4 w-[75%] mx-auto mt-[13%]   " />
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,19 @@ const Navbar = () => {
   return (
     <div className="flex-none order-1 flex-grow-0 w-54 h-32 relative">
       <img
+        src="../../src/assets/images/Nav mobile.png"
+        alt=""
+        className="absolute top-5 pr-2 block sm:hidden " // screen solo para pantallas pequeñas
+      />
+      <img
         src="../../src/assets/images/Component.png"
         alt=""
-        className="absolute top-5 pr-2 block xl:hidden "
+        className="absolute top-5 pr-2 hidden md:block xl:hidden " // screem para pantallas medias y no se visualiza en pequeñas sin grande
       />
-
       <img
         src="../../src/assets/images/Component2-1.png"
         alt=""
-        className="absolute top-5  xl:pr-2 hidden xl:block "
+        className="absolute top-5  xl:pr-2 hidden xl:block " // screen solo para pantalla grande no se visualiza en las demas
       />
     </div>
   );
