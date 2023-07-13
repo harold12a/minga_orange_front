1da6ad70552f71470e0b065df3f3305e8878fec2
andreaprada1993 <andrea_prada_93@hotmail.com>
2023-07-13 18:57:25 -0500

    cambios realizados en su totalidad de sprind 2

--- src/pages/NotAllowed.jsx
+++ src/pages/NotAllowed.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 
 
-const NotAllowed = () =>{
+const NotAllowed = () => {
 
-    return(
-<>
-<main className="">
-    <h1>Not Allowed</h1>
-<img className=""
-src="../../src/assets/images/Not_allowed.png"
- />
- 
-</main>
-</>
+    return (
+
+        <main className='flex flex-col md:justify-center'>
+            <h1 className='text-[30px] items-center mt-20 p-10 text-white bg-red-800 md:text-center md:p-20 '>Not allowed. This is not your role. Go back, you're lost.</h1>
+            <img className='w-[500px] h-[300px] items-center md:items-center md:w-[800px] md:h-[500px]'
+                src="../../src/assets/images/Not_allowed.png"
+            />
+        </main>
+
 
     )
 }
