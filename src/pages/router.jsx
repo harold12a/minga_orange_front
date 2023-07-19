a43276d59cc856dd961cf278c6141bc5d1f6de30
Andrea Prada Gutierrez <125933648+andreaprada1993@users.noreply.github.com>
2023-07-19 08:12:01 -0500

    M04 alerts (#31)
    * crear nueva rama
    
    * arreglos del front de los mensajes y vincular a chapters con el back por que se estaba llamando mal
    
    * actualizar los cambios
    
    * cambios realizados en su totalidad de sprind 2

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -6,6 +6,7 @@ import Resgister from "./Resgister";
 import FormNewMangas from "./FormNewMangas";
 import AuthorForm from "./AuthorForm.jsx";
 import ChapterForm from "./ChapterForm";
+import NotAllowed from "./NotAllowed";
 
 
 
@@ -29,17 +30,17 @@ const router = createBrowserRouter([
       { path: "/manga-form", element: <FormNewMangas /> ,loader: ()=>{
         let user = JSON.parse(localStorage.getItem('user'))
         console.log(user);
-         return ( user.role === 0 || user.role === 3) &&  redirect('/')
+         return ( user.role === 0 || user.role === 3) &&  redirect('/not-allowed')
       }  },
       { path: "/author-form", element: <AuthorForm />,loader: ()=>{
         let user = JSON.parse(localStorage.getItem('user'))
         console.log(user);
-         return (user.role === 1 || user.role === 2 || user.role === 3  ) &&  redirect('/')
+         return (user.role === 1 || user.role === 2 || user.role === 3  ) &&  redirect('/not-allowed')
       } },
       { path: "/:manga_id/chapter-form",element: <ChapterForm />,loader: ()=>{
         let user = JSON.parse(localStorage.getItem('user'))
         console.log(user);
-         return (user.role === 3 || user.role === 0  ) &&  redirect('/')
+         return (user.role === 3 || user.role === 0  ) &&  redirect('/not-allowed')
       }},
       // NO VA PARA EL GRUPO ORANGE
       // { path: "/cia-form", element: <CompanyForm />,loader: ()=>{
@@ -47,6 +48,7 @@ const router = createBrowserRouter([
       //   console.log(user);
       //    return (user.role === 1 || user.role === 2 || user.role === 3  ) &&  redirect('/')
       // } },
+      {path: "/not-allowed",element: <NotAllowed />}
     ]
   },
 ]);
