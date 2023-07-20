3ad8cbe3e95dcf0558f54986078741a4e8619f0f
harold alzate <haroldalzate99@gmail.com>
2023-07-20 15:54:38 -0500

    modified:   src/pages/Mangas.jsx

--- src/pages/Mangas.jsx
+++ src/pages/Mangas.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { RiSearch2Line } from "react-icons/ri";
-import { Link as Anchor } from 'react-router-dom'
+import { Link as Anchor } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import apiUrl from "../apiUrl";
+import header from "../header";
 
 const Mangas = () => {
+  const [title, setTitle] = useState('');
+  const [mangas, setMangas] = useState([]);
+  const [prev, setPrev] = useState(null);
+  const [next, setNext] = useState(null);
+  useEffect(() => {
+    try {
+      console.log(title);
+      axios(apiUrl + "mangas?title="+ title, header()).then((res) => {
+        setMangas(res.data.response);
+        setPrev(res.data.prev_page);
+        setNext(res.data.next_page);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [title]);
+
   return (
     <>
       <main className="  bg-[url('../../src/assets/images/pexels-lisa.jpg')] bg-cover   absolute   lg:h-[70%]   bg-center mt-[-18%] xl:mt-[0%]   h-[50%] w-full  xl:w-[100%]    ">
@@ -16,46 +37,53 @@ const Mangas = () => {
             name="insertManga"
             id="insertManga"
             placeholder="Find your manga here"
+            onChange={(event)=>setTitle(event.target.value)}
           />
           <RiSearch2Line className="w-6 xl:w-10 h-6 mt-[-42px] ml-[15%] md:ml-[33%] xl:ml-[22%] text-indigo-700 xl:text-gray-500" />
         </div>
-     
-          <div className=" h-screen  bg-[#EBEBEB] rounded-t-[70px] xl:rounded-t-[20px] mt-[80px] xl:w-[95%] xl:ml-[2.5%]">
-            <div className=" flex justify-center xl:justify-start xl:ml-[12%] " >
+
+        <div className=" h-screen  bg-[#EBEBEB] rounded-t-[70px] xl:rounded-t-[20px] mt-[80px] xl:w-[95%] xl:ml-[2.5%]">
+          <div className=" flex justify-center xl:justify-start xl:ml-[12%] ">
             <Anchor className=" hidden xl:block bg-gray-400 text-gray-800 pt-[7px] md:pt-[10px]  pl-[15px] md:pl-[40px] mx-[2%] mt-[15%] xl:mt-[5%]  w-[70px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
-            all
-          </Anchor>
-            <Anchor className=" bg-red-200 text-red-500 pt-[7px] md:pt-[10px]  pl-[15px] md:pl-[26px] mx-[2%] xl:mx-[-1%] mt-[15%] xl:mt-[5%] w-[70px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
-            Shonen
-          </Anchor>
-            <Anchor className=" bg-orange-200 text-orange-500 pt-[7px] md:pt-[10px] pl-[13px] md:pl-[26px] mx-[2%]  mt-[15%] xl:mt-[5%]  w-[60px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
-            Seinen
-          </Anchor>
-            <Anchor className=" bg-green-200 text-green-500 pt-[7px] md:pt-[10px]  pl-[15px] md:pl-[26px] mx-[2%] xl:mx-[-1%] mt-[15%] xl:mt-[5%] w-[60px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
-          Shojo
-          </Anchor>
-            <Anchor className=" bg-purple-200 text-purple-500 pt-[7px] md:pt-[10px] pl-[15px] md:pl-[26px] mx-[2%]  mt-[15%] xl:mt-[5%]  w-[70px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
-            kodomo
+              all
             </Anchor>
-            
-            </div>
-
-            <div className="bg-white rounded-[20px] mt-[30px] h-[25%] w-[90%] mx-[5%]  xl:ml-[12%] xl:w-[35%]  " >
-              <h1 className="font-bold grid grid-cols-3 gap-2 text-[18px] md:text-[29px] pl-[30px] pt-8 " >
-                Naruto:And That´s Why Yout´re Disqualified!!#8
-              </h1>
-              <h2 className="text-orange-400 font-bold pl-[30px] md:text-[25px]" >
-                Type
-              </h2>
-              <img className="  mt-[-47%] sm:mt-[-35%] xl:mt-[-38%] h-[100%] w-[50%] ml-[50%] object-cover rounded-l-[50%] rounded-r-[10%] " src="../../src/assets/images/card-naruto.png" alt="img-naruto" />
-
-            </div>
-        
-         
-
+            <Anchor className=" bg-red-200 text-red-500 pt-[7px] md:pt-[10px]  pl-[15px] md:pl-[26px] mx-[2%] xl:mx-[-1%] mt-[15%] xl:mt-[5%] w-[70px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
+              Shonen
+            </Anchor>
+            <Anchor className=" bg-orange-200 text-orange-500 pt-[7px] md:pt-[10px] pl-[13px] md:pl-[26px] mx-[2%]  mt-[15%] xl:mt-[5%]  w-[60px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
+              Seinen
+            </Anchor>
+            <Anchor className=" bg-green-200 text-green-500 pt-[7px] md:pt-[10px]  pl-[15px] md:pl-[26px] mx-[2%] xl:mx-[-1%] mt-[15%] xl:mt-[5%] w-[60px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
+              Shojo
+            </Anchor>
+            <Anchor className=" bg-purple-200 text-purple-500 pt-[7px] md:pt-[10px] pl-[15px] md:pl-[26px] mx-[2%]  mt-[15%] xl:mt-[5%]  w-[70px] md:w-[100px] h-[35px] md:h-[45px] rounded-[50000px] text-[12px] md:text-[15px]">
+              kodomo
+            </Anchor>
           </div>
-      
+
+          <div className="bg-white rounded-[20px] mt-[30px] h-[25%] w-[90%] mx-[5%]  xl:ml-[12%] xl:w-[35%]  ">
+            <h1 className="font-bold grid grid-cols-3 gap-2 text-[18px] md:text-[29px] pl-[30px] pt-8 ">
+              Naruto:And That´s Why Yout´re Disqualified!!#8
+            </h1>
+            <h2 className="text-orange-400 font-bold pl-[30px] md:text-[25px]">
+              Type
+            </h2>
+            
+            <img
+              className="  mt-[-47%] sm:mt-[-35%] xl:mt-[-38%] h-[100%] w-[50%] ml-[50%] object-cover rounded-l-[50%] rounded-r-[10%] "
+              src="../../src/assets/images/card-naruto.png"
+              alt="img-naruto"
+            />
+          </div>
+          <div>
+            {prev && <input type="button" value={'previus page'}/> }
+            {next && <input type="button" value={'next page'}/> }
+          </div>
+         
+        </div>
+     
       </main>
+
     </>
   );
 };
