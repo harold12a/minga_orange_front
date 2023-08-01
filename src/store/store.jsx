1aefdd8f513481c9b93ea5879da418f9b18517d9
Mauricio Pantoja <47827317+pantojamao@users.noreply.github.com>
2023-08-01 11:08:41 -0500

    correccion (#41)

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,19 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mangaReducer from "./reducers/manga";
 import chapterReducer from "./reducers/chapters";
-import mangaReducer from "./reducers/mangas";
+import mangasnewsReducer from './reducers/mangas'
 
 const store = configureStore({
     reducer:{
-     
-       manga: mangaReducer
-
-const store = configureStore({
-    reducer:{
-    mangas: mangaReducer,
-    chapter: chapterReducer
-    
+        //acá vamos a traernos los reductores de los diferentes recursos (usuarios, mangas capitulos...)
+        chapter: chapterReducer,
+        manga: mangaReducer,
+        mangasnews: mangasnewsReducer
     }
 })
 
-export default store;
+export default store
